2ad3f0232b19d414b23e72811d7d701433e46dbf
Gopika Kadiyala <gopikakadiyala@Gopikas-MacBook-Air.local>
2025-08-19 00:39:01 -0500

    Task_2

--- main.go
+++ main.go
@@ -5,8 +5,22 @@ import (
 	"fmt"
 )
 
+// --- Declaration---Structures -----
+type College struct {
+	Name  string
+	Count int
+}
+
 func main() {
+	// hello world
+
 	fmt.Println("Hello World")
+
+	//----Slice-Declaration----
+	slice := []int{1, 2, 3}
+	fmt.Println(slice)
+
+	// int......
 
 	a, b := 7, 2
 	fmt.Println(simplecalc.Add(a, b))
@@ -54,10 +68,70 @@ func main() {
 
 	// --- defer  ---
 
-	defer fmt.Println("Second")
 	defer fmt.Println("Middle")
 	defer fmt.Println("First")
+	defer fmt.Println("Second")
+	//---------------//-------------------//
+	// ------Structure call---- --- Structure Example ----
 
+	college := College{
+		Name:  "NIU",
+		Count: 4000,
+	}
+	fmt.Println(college.Name)
+	fmt.Println(college.Count)
+	fmt.Println(college)
+
+	//--- Arrays -- Fixed length seq of num ---
+
+	//Array--INT---
+
+	var A [7]int
+	fmt.Println(A) //Default values = 0//
+
+	A[4] = 8
+	fmt.Println(A)
+
+	//Array--STRING-
+
+	B := [5]string{"Northern", "Illinois", "University", "NIU", "Chicago"}
+
+	fmt.Println(B)
+
+	//--Array--Range-----
+
+	for i, v := range B {
+		fmt.Println(i, v)
+	}
+
+	//---Slices---
+	//----len & cap------
+
+	m := []int{1, 2, 3}
+	fmt.Println(len(m))
+	fmt.Println(cap(m))
+	fmt.Println(m)
+
+	g := [6]int{3, 4, 5, 6, 7, 8}
+	slice = g[2:4] ////Slice from index 2 to 3
+	fmt.Println(slice)
+	fmt.Println(g)
+
+	n := [5]int{3, 4, 5, 6, 7}
+	slice = n[:4] //Slice from beginning till index 3
+	fmt.Println(slice)
+	fmt.Println(n)
+
+	o := [9]int{1, 2, 3, 4, 5, 6, 7, 8}
+	slice = o[4:] //Slice from index 4 till end
+	fmt.Println(slice)
+	fmt.Println(o)
+
+	//-----append-----
+	s := make([]int, 4, 8) //// slice with length=4, capacity=8
+	s = append(s, 4)       // Add element at the end
+	s = append(s, 3)
+	fmt.Println(s)
 }
 
 func increase(num *int) {
